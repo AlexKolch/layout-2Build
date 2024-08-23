14ea6b35d04477a9fc213eef881425728d8264af
Alexey Kolch <alexeykolychenkov94@gmail.com>
2024-08-23 13:32:30 +0300

    Initial Commit

---
+++ layout#2Build/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layout#2Build
+//
+//  Created by Алексей Колыченков on 23.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
